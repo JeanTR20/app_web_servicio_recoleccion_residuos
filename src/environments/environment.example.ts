8dfcb85f8dc5eeaaa9bb84ed4eedc2aeff24ee49
Jean <jeantorresricse@gmail.com>
2025-03-22 21:31:40 -0500

    build(environment): modificar environment.ts y agregar environment.example.ts

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,15 @@
+export const environment = {
+
+  baseUrl: 'http://localhost:3000/api/v1',
+
+  firebase: {
+    projectId: "",
+    appId: "",
+    storageBucket: "",
+    locationId: "",
+    apiKey: "",
+    authDomain: "",
+    messagingSenderId: ""
+  }
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,15 @@
 export const environment = {
 
-  baseUrl: process.env['APP_BASE_URL'] ?? 'http://localhost:3000/api/v1',
+  baseUrl: 'http://localhost:3000/api/v1',
 
   firebase: {
-    projectId: process.env['APP_FIREBASE_PROJECT_ID'],
-    appId: process.env['FIREBASE_APP_ID'],
-    storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
-    locationId: process.env['FIREBASE_LOCATION_ID'],
-    apiKey: process.env['FIREBASE_API_KEY'],
-    authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
-    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID']
+    projectId:"proyectorecoleccionbasura",
+    appId:"1:663591349192:web:64214a7b5bb207693e2efd",
+    storageBucket:"proyectorecoleccionbasura.appspot.com",
+    locationId:"us-central",
+    apiKey:"AIzaSyBRYOwrzNo0427tK5PVcj94pXrFAzegGcI",
+    authDomain:"proyectorecoleccionbasura.firebaseapp.com",
+    messagingSenderId:"663591349192"
   }
 
 }
